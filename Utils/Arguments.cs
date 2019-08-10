a8ec7cf37c5d1b5c40ea44635f02f3a5a4d1eb7f
Bluscream <admin@timo.de.vc>
2019-08-10 07:52:12 +0200

    oWo

--- Utils/Arguments.cs
+++ Utils/Arguments.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,42 +13,43 @@ namespace VRChatLauncher.Utils
         public const string VRCToolsPrefix = "vrctools.";
     }
     public class LauncherArguments {
-        public Argument NoSSL = new Argument(ArgType.Launcher) { ValueType = typeof(bool), DefaultValue = false, Name = "nossl", Description = "Instructs the launcher to ignore SSL errors" };
-        public Argument KeepOpen = new Argument(ArgType.Launcher) { ValueType = typeof(bool), DefaultValue = false, Name = "keep", Description = "Keeps the launcher open even if another instance of it is already running" };
-        public Argument Verbose = new Argument(ArgType.Launcher) { ValueType = typeof(bool), DefaultValue = false, Name = "verbose", Description = "Starts logging Trace messages" };
-        public Argument Console = new Argument(ArgType.Launcher) { ValueType = typeof(bool), DefaultValue = false, Name = "console", Description = "Opens a console on startup" };
+        public Argument IgnoreSSLErrors = new LauncherArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "nossl", Description = "Instructs the launcher to ignore SSL errors" };
+        public Argument KeepOpen = new LauncherArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "keep", Description = "Keeps the launcher open even if another instance of it is already running" };
+        public Argument Verbose = new LauncherArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "verbose", Description = "Starts logging Trace messages" };
+        public Argument Console = new LauncherArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "console", Description = "Opens a console on startup" };
+        public Argument Skip = new LauncherArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "skiplauncher", Description = "Skip opening the launcher and start the game directly" };
     }
     public class VRCTArguments {
-        public Argument ForceUpdate = new Argument(ArgType.VRCTools) { ValueType = typeof(bool), DefaultValue = false, Name = "forceupdate", Description = "Forces a vrctools update" };
-        public Argument NoUpdate = new Argument(ArgType.VRCTools) { ValueType = typeof(bool), DefaultValue = false, Name = "noupdate", Description = "Skips VRCTools and AvatarFav update check (Required to run a modified version of VRCTools)" };
-        public Argument EnableNamePlateIcons = new Argument(ArgType.VRCTools) { ValueType = typeof(bool), DefaultValue = false, Name = "enablenameplateicons", Description = "Shows nameplate icons for VRCTools users" };
-        public Argument Dev = new Argument(ArgType.VRCTools) { ValueType = typeof(bool), DefaultValue = false, Name = "dev", Description = "Changes VRCMN Server port from  26342 to 26345" };
-        public Argument Verbose = new Argument(ArgType.Unknown) { ValueType = typeof(bool), DefaultValue = false, Name = "verbose", Description = "Enables VRCTools Console" };
-        public Argument NoModLoader = new Argument(ArgType.Unknown) { ValueType = typeof(bool), DefaultValue = false, Name = "nomodloader", Description = "Disables vrctools mod loading" };
+        public Argument ForceUpdate = new VRCTArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "forceupdate", Description = "Forces a vrctools update" };
+        public Argument NoUpdate = new VRCTArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "noupdate", Description = "Skips VRCTools and AvatarFav update check (Required to run a modified version of VRCTools)" };
+        public Argument EnableNamePlateIcons = new VRCTArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "enablenameplateicons", Description = "Shows nameplate icons for VRCTools users" };
+        public Argument Dev = new VRCTArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "dev", Description = "Changes VRCMN Server port from  26342 to 26345" };
+        public Argument Verbose = new Argument() { ValueType = typeof(bool), DefaultValue = false, Name = "verbose", Description = "Enables VRCTools Console" };
+        public Argument NoModLoader = new Argument() { ValueType = typeof(bool), DefaultValue = false, Name = "nomodloader", Description = "Disables vrctools mod loading" };
         // public Argument NoTitle = new Argument() { ValueType = typeof(bool), DefaultValue = false, Name = "notitle", Description = "Disables modifying the title of your game window" };
     }
     public class GameArguments {
-        public Argument FPS = new Argument(ArgType.Game) { ValueType = typeof(int), DefaultValue = null, Name = "fps", Description = "Sets the max FPS the game will lock on" };
-        public Argument NoVR = new Argument(ArgType.Game) { ValueType = typeof(bool), DefaultValue = false, Name = "no-vr", Description = "Sets the game to desktop-mode" };
-        public Argument OSC = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "osc", Description = "Set additional arguments for osc" };
-        public Argument URL = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "url", Description = "Same as applying a URL without this param" };
-        public Argument LogDebugLevels = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "log-debug-levels", Description = "Sets log levels for the game" };
-        public Argument ClearPlayerModerations = new Argument(ArgType.Game) { ValueType = typeof(bool), DefaultValue = false, Name = "clear-player-moderations", Description = "Clears all player moderations, be careful with this!" };
-        public Argument AutoLoginUsername = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "auto-login-un", Description = "Sets username for auto login" };
-        public Argument AutoLoginPassword = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "auto-login-pw", Description = "Sets password for auto login" };
-        public Argument AutoLogin2FA = new Argument(ArgType.Game) { ValueType = typeof(string), Name = "auto-login-2fa", Description = "Sets two factor authentication code for auto login" };
-        public Argument SpawnRadius = new Argument(ArgType.Game) { ValueType = typeof(string), DefaultValue = null, Name = "spawn-radius", Description = "N/A" };
-        public Argument Profile = new Argument(ArgType.Game) { ValueType = typeof(string), DefaultValue = null, Name = "profile", Description = "N/A" };
+        public Argument FPS = new GameArgument() { ValueType = typeof(int), DefaultValue = null, Name = "fps", Description = "Sets the max FPS the game will lock on" };
+        public Argument NoVR = new GameArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "no-vr", Description = "Sets the game to desktop-mode" };
+        public Argument OSC = new GameArgument() { ValueType = typeof(string), Name = "osc", Description = "Set additional arguments for osc" };
+        public Argument URL = new GameArgument() { ValueType = typeof(string), Name = "url", Description = "Same as applying a URL without this param" };
+        public Argument LogDebugLevels = new GameArgument() { ValueType = typeof(string), Name = "log-debug-levels", Description = "Sets log levels for the game" };
+        public Argument ClearPlayerModerations = new GameArgument() { ValueType = typeof(bool), DefaultValue = false, Name = "clear-player-moderations", Description = "Clears all player moderations, be careful with this!" };
+        public Argument AutoLoginUsername = new GameArgument() { ValueType = typeof(string), Name = "auto-login-un", Description = "Sets username for auto login" };
+        public Argument AutoLoginPassword = new GameArgument() { ValueType = typeof(string), Name = "auto-login-pw", Description = "Sets password for auto login" };
+        public Argument AutoLogin2FA = new GameArgument() { ValueType = typeof(string), Name = "auto-login-2fa", Description = "Sets two factor authentication code for auto login" };
+        public Argument SpawnRadius = new GameArgument() { ValueType = typeof(string), DefaultValue = null, Name = "spawn-radius", Description = "N/A" };
+        public Argument Profile = new GameArgument() { ValueType = typeof(string), DefaultValue = null, Name = "profile", Description = "N/A" };
     }
-    /*public class VRCTArgument : Argument {
-        public new Constants.ArgType ArgType = Constants.ArgType.VRCTools;
+    public class VRCTArgument : Argument {
+        public new ArgType ArgType = ArgType.VRCTools;
     }
     public class GameArgument : Argument {
-        public new Constants.ArgType ArgType = Constants.ArgType.Game;
+        public new ArgType ArgType = ArgType.Game;
     }
     public class LauncherArgument : Argument {
-        public new Constants.ArgType ArgType = Constants.ArgType.Launcher;
-    }*/
+        public new ArgType ArgType = ArgType.Launcher;
+    }
     public class Argument
     {
         public ArgType ArgType; // = Constants.ArgType.Unknown;
@@ -55,25 +57,38 @@ namespace VRChatLauncher.Utils
         public string Name;
         public string Description;
         public List<object> Values;
-        public object Value { get { return Value; } set { Value = Value; IsSet = true; } }
+        public object Value;
         public object DefaultValue;
         public bool IsSet = false;
-        public Argument(ArgType argType) { ArgType = argType; }
+        // public Argument(ArgType argType) { ArgType = argType; }
+        public void SetValue(object value)
+        {
+            Value = value;
+            IsSet = true;
+        }
     }
     public class Arguments
     {
-        public LauncherArguments Launcher;
-        public GameArguments Game;
-        public VRCTArguments VRCTools;
+        public LauncherArguments Launcher = new LauncherArguments();
+        public GameArguments Game = new GameArguments();
+        public VRCTArguments VRCTools = new VRCTArguments();
         public List<IPC.Game.Command> Commands;
-        public string String { get { return ""; } }
-        public string[] Array { get { return new string[] { }; } }
-        public List<string> List { get { return new List<string>(); } }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public string[] ToArray()
+        {
+            return new string[] { };
+        }
+        public List<string> ToList()
+        {
+            return new List<string>();
+        }
         public static Arguments FromArgs(List<string> args)
         {
             Logger.Log("Parsing Arguments:", args.ToJson());
             var arguments = new Arguments();
-            arguments.Launcher = new LauncherArguments();
             // arguments.Game = new GameArguments();
             // arguments.VRCTools = new VRCTArguments();
             // arguments.Commands = new List<IPC.Game.Command>();
@@ -101,12 +116,21 @@ namespace VRChatLauncher.Utils
                 switch (argType)
                 {
                     case ArgType.Launcher:
-                        switch (curArg)
+                        /* PropertyInfo[] properties = typeof(LauncherArguments).GetProperties();
+                        foreach (PropertyInfo property in properties)
                         {
+                            if (property.GetGetMethod())
+                            property.SetValue(record, value);
+                        }*/ 
+                        switch (curArg) {
                             case "keep":
-                                arguments.Launcher.KeepOpen.Value = true; break;
+                                arguments.Launcher.KeepOpen.SetValue(true); break;
                             case "nossl":
-                                arguments.Launcher.NoSSL.Value = true; break;
+                                arguments.Launcher.IgnoreSSLErrors.SetValue(true); break;
+                            case "console":
+                                arguments.Launcher.Console.SetValue(true); break;
+                            case "verbose":
+                                arguments.Launcher.Verbose.SetValue(true); break;
                             default:
                                 break;
                         }
@@ -114,6 +138,12 @@ namespace VRChatLauncher.Utils
                     case ArgType.Game:
                         break;
                     case ArgType.VRCTools:
+                        switch (curArg) {
+                            case "enablenameplateicons":
+                                arguments.VRCTools.EnableNamePlateIcons.SetValue(true); break;
+                            default:
+                                break;
+                        }
                         break;
                     default:
                         break;
